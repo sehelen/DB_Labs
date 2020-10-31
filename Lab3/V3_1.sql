382758da93c78c50dc6657db5d65355ce32c3e01
Elena Seliun <eseliun@mail.ru>
2020-10-31 16:45:18 +0300

    add lab 3

---
+++ Lab3/V3_1.sql
@@ -0,0 +1,78 @@
+﻿USE AdventureWorks2012;
+
+-- a) добавьте в таблицу dbo.Address поле AddressType типа nvarchar размерностью 50 символов;
+ALTER TABLE dbo.Address 
+ADD AddressType VARCHAR(50) NULL;
+GO
+
+-- b) объявите табличную переменную с такой же структурой как dbo.Address и заполните ее данными из dbo.Address. Заполните поле AddressType значениями из Person.AddressType поля Name;
+DECLARE @TempTable TABLE
+             (
+                 AddressID       INT          NOT NULL,
+                 AddressLine1    nvarchar(60) not null,
+                 AddressLine2    nvarchar(60),
+                 City            nvarchar(20) not null,
+                 StateProvinceID int          not null,
+                 PostalCode      nvarchar(15) not null,
+                 ModifiedDate    datetime     not null,
+                 AddressType     nvarchar(50)
+             );
+
+INSERT INTO @TempTable(AddressID, AddressLine1, AddressLine2, City, StateProvinceID, PostalCode, ModifiedDate, AddressType)
+SELECT AddressID,
+       AddressLine1,
+       AddressLine2,
+       City,
+       StateProvinceID,
+       PostalCode,
+       ModifiedDate,
+	   (Select Name
+			FROM AdventureWorks2012.Person.AddressType
+            JOIN AdventureWorks2012.Person.BusinessEntityAddress AS A
+            ON AddressType.AddressTypeID = A.AddressTypeID
+			WHERE dbo.Address.AddressID = A.AddressID)
+FROM dbo.Address;
+
+-- c) обновите поле AddressType в dbo.Address данными из табличной переменной. Также обновите AddressLine2, если значение в поле NULL — обновите поле данными из AddressLine1;
+UPDATE dbo.Address
+SET dbo.Address.AddressType = [@TempTable].AddressType
+FROM @TempTable
+WHERE dbo.Address.AddressID = [@TempTable].AddressID;
+GO
+
+UPDATE dbo.Address
+SET dbo.Address.AddressLine2 = Address.AddressLine1
+FROM dbo.Address
+WHERE dbo.Address.AddressLine2 is null
+GO
+
+-- d) удалите данные из dbo.Address, оставив только по одной строке для каждого AddressType с максимальным AddressID;
+DELETE FROM dbo.Address
+WHERE AddressID NOT IN (SELECT MAX(AddressID) FROM Address GROUP BY Address.AddressType);
+GO
+
+-- e) удалите поле AddressType из таблицы, удалите все созданные ограничения и значения по умолчанию.
+ALTER TABLE dbo.Address
+DROP COLUMN AddressType
+GO
+
+SELECT *
+FROM AdventureWorks2012.INFORMATION_SCHEMA.CONSTRAINT_TABLE_USAGE
+WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'Address';
+GO
+
+ALTER TABLE dbo.Address
+    DROP CONSTRAINT [PK_Address];
+GO
+
+ALTER TABLE dbo.Address
+    DROP CONSTRAINT [VLDT_Address_PostalCode];
+GO
+
+ALTER TABLE dbo.Address
+    DROP CONSTRAINT [DFLT_Address_ModifiedDate];
+GO
+
+-- f) удалите таблицу dbo.Address
+DROP TABLE dbo.Address;
+GO
---
+++ Lab3/V3_2.sql
@@ -0,0 +1,114 @@
+﻿/*
+a) выполните код, созданный во втором задании второй лабораторной работы. 
+Добавьте в таблицу dbo.Address поля CountryRegionCode NVARCHAR(3) и TaxRate SMALLMONEY. 
+Также создайте в таблице вычисляемое поле DiffMin, считающее разницу между значением в поле TaxRate 
+и минимальной налоговой ставкой 5.00.
+*/
+ALTER TABLE dbo.Address 
+ADD CountryRegionCode NVARCHAR(3), 
+	TaxRate SMALLMONEY, 
+	DiffMin as TaxRate - 5.00;
+GO
+
+/*
+b) создайте временную таблицу #Address, с первичным ключом по полю AddressID.
+Временная таблица должна включать все поля таблицы dbo.Address за исключением поля DiffMin.
+*/
+CREATE TABLE #Address
+(
+    AddressID           INT          NOT NULL,
+    AddressLine1        nvarchar(60) not null,
+    AddressLine2        nvarchar(60),
+    City                nvarchar(30) not null,
+    StateProvinceID     int          not null,
+    PostalCode          nvarchar(15) not null,
+    ModifiedDate        datetime     not null,
+    [CountryRegionCode] nvarchar(3),
+    [TaxRate]           SMALLMONEY
+);
+GO
+
+ALTER TABLE #Address
+	ADD CONSTRAINT PK_Address_AddressID 
+	PRIMARY KEY ([AddressID]);
+GO
+
+/*
+c) заполните временную таблицу данными из dbo.Address. 
+Поле CountryRegionCode заполните значениями из таблицы Person.StateProvince. 
+Поле TaxRate заполните значениями из таблицы Sales.SalesTaxRate. 
+Выберите только те записи, где TaxRate > 5. 
+Выборку данных для вставки в табличную переменную осуществите в Common Table Expression (CTE).
+*/
+WITH [ADDRESS_TABLE] AS (SELECT AddressID,
+                      AddressLine1,
+                      AddressLine2,
+                      City,
+                      dbo.Address.StateProvinceID,
+                      PostalCode,
+                      dbo.Address.ModifiedDate,
+                      SP.CountryRegionCode,
+                      STR.TaxRate
+               FROM dbo.Address
+                        JOIN AdventureWorks2012.Person.StateProvince as SP
+                             ON dbo.Address.StateProvinceID = SP.StateProvinceID
+                        JOIN AdventureWorks2012.Sales.SalesTaxRate as STR ON SP.StateProvinceID = STR.StateProvinceID
+               WHERE STR.TaxRate > 5
+)
+INSERT
+INTO #Address (AddressID, AddressLine1, AddressLine2, City, StateProvinceID, PostalCode, ModifiedDate,
+                   CountryRegionCode, TaxRate)
+SELECT AddressID,
+       AddressLine1,
+       AddressLine2,
+       City,
+       StateProvinceID,
+       PostalCode,
+       ModifiedDate,
+       CountryRegionCode,
+       TaxRate
+FROM [ADDRESS_TABLE];
+GO
+
+/*
+d) удалите из таблицы dbo.Address одну строку (где StateProvinceID = 36)
+*/
+DELETE FROM dbo.Address
+	WHERE [StateProvinceID] = 36;
+GO
+
+/*
+e) напишите Merge выражение, использующее dbo.Address как target, а временную таблицу как source. 
+Для связи target и source используйте AddressID. Обновите поля CountryRegionCode и TaxRate, 
+если запись присутствует в source и target. Если строка присутствует во временной таблице, 
+но не существует в target, добавьте строку в dbo.Address. Если в dbo.Address присутствует такая строка, 
+которой не существует во временной таблице, удалите строку из dbo.Address.
+*/
+MERGE INTO dbo.Address AS base
+USING #Address AS source
+ON base.AddressID = source.AddressID
+WHEN MATCHED THEN
+    UPDATE
+    SET CountryRegionCode = source.CountryRegionCode,
+        TaxRate           = source.TaxRate
+WHEN NOT MATCHED BY TARGET THEN
+    INSERT (AddressID,
+            AddressLine1,
+            AddressLine2,
+            City,
+            StateProvinceID,
+            PostalCode,
+            ModifiedDate,
+            CountryRegionCode,
+            TaxRate)
+    VALUES (source.AddressID,
+            source.AddressLine1,
+            source.AddressLine2,
+            source.City,
+            source.StateProvinceID,
+            source.PostalCode,
+            source.ModifiedDate,
+            source.CountryRegionCode,
+            source.TaxRate)
+WHEN NOT MATCHED BY SOURCE THEN DELETE;
+GO
